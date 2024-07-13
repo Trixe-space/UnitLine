8b497a71367d09f8349490a17fef7ebeb468ae01
Trixe space <trixe-space@trixe-space.tk>
2024-07-13 13:06:15 +0600

    Refactored code in index, created even better error handling and added ls and help commands.

--- dist/index.js
+++ dist/index.js
@@ -24,34 +24,113 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const console_1 = require("console");
 const distanceConverters = __importStar(require("./unit_converters/distance"));
-let input = process.argv;
-let unitType = input[2].slice(1);
-let currentUnit = input[3].split('-')[0];
-let convertedUnit = input[3].split('-')[1];
-let value = parseFloat(input[4]);
-let distanceConverter = new Map([
-    ['km', distanceConverters.toKilometer(currentUnit, value)],
-    ['hm', distanceConverters.toHectometer(currentUnit, value)],
-    ['dam', distanceConverters.toDecameter(currentUnit, value)],
-    ['m', distanceConverters.toMeter(currentUnit, value)],
-    ['dm', distanceConverters.toDecimeter(currentUnit, value)],
-    ['cm', distanceConverters.toCentimeter(currentUnit, value)],
-    ['mm', distanceConverters.toMillimeter(currentUnit, value)],
-    ['um', distanceConverters.toMicrometer(currentUnit, value)],
-    ['nm', distanceConverters.toNanometer(currentUnit, value)]
-]);
-switch (unitType) {
-    case 'distance':
-    case 'd':
-        if (distanceConverter.has(convertedUnit)) {
-            console.log(`${value + currentUnit} -> ${distanceConverter.get(convertedUnit) + convertedUnit}`);
-        }
-        else {
-            throw (0, console_1.error)(`${convertedUnit} is not a valid unit`);
-        }
-        break;
-    default:
-        throw (0, console_1.error)(`${unitType} is not a valid type`);
+const input = process.argv;
+const helpMessage = `
+unitline <command>
+`;
+const distanceUnits = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm', 'um', 'nm'];
+const output = (unitFrom, value, unitTo, convertedValue) => {
+    console.log(`${value + unitFrom} -> ${convertedValue + unitTo}`);
+};
+//TODO Put all the messages in their own seperate file.
+const converter = (unitType, unitFrom, unitTo, value) => {
+    let convertedValue;
+    const distanceConverter = new Map([
+        ['km', distanceConverters.toKilometer(unitFrom, value)],
+        ['hm', distanceConverters.toHectometer(unitFrom, value)],
+        ['dam', distanceConverters.toDecameter(unitFrom, value)],
+        ['m', distanceConverters.toMeter(unitFrom, value)],
+        ['dm', distanceConverters.toDecimeter(unitFrom, value)],
+        ['cm', distanceConverters.toCentimeter(unitFrom, value)],
+        ['mm', distanceConverters.toMillimeter(unitFrom, value)],
+        ['um', distanceConverters.toMicrometer(unitFrom, value)],
+        ['nm', distanceConverters.toNanometer(unitFrom, value)]
+    ]);
+    switch (unitType) {
+        case 'd':
+        case 'distance':
+            if (distanceConverter.has(unitTo)) {
+                output(unitFrom, value, unitTo, distanceConverter.get(unitTo));
+            }
+            else {
+                console.log(`
+${unitTo} is not a valid unit to convert to.
+
+List of valid units: ${distanceUnits.toString()}
+
+To check additional units, run:
+    unitline ls
+                    `);
+            }
+            break;
+        default:
+            console.log(`
+${unitType} is not a valid type
+
+List of available types:
+d, distance       Convert units of distance
+                `);
+    }
+};
+if (input.length >= 3) {
+    const command = input[2];
+    switch (command) {
+        case 'help':
+        case '--help':
+        case '-help':
+            console.log(helpMessage);
+            break;
+        case 'ls':
+        case 'list':
+            const argument = input[3];
+            switch (argument) {
+                case '-d':
+                case '-distance':
+                    console.log(distanceUnits.toString());
+                    break;
+                default:
+                    console.log(`
+Distance: ${distanceUnits.toString()}
+                        `);
+            }
+            break;
+        case 'conv':
+        case 'convert':
+            if (input.length <= 3) {
+                console.log(`
+Not enough arguments.
+
+Usage:
+unitline conv <type of units> <unit from>-<unit to> <value>
+
+Unit Types:
+d, distance       Convert units of distance
+
+To view supported units, run:
+    unitline ls
+                    `);
+                break;
+            }
+            let unitType = input[3];
+            let unitFrom = input[4].split('-')[0];
+            let unitTo = input[4].split('-')[1];
+            let value = parseFloat(input[5]);
+            if (isNaN(value)) {
+                console.log(`The value provided is not a number`);
+                break;
+            }
+            converter(unitType, unitFrom, unitTo, value);
+            break;
+        default:
+            console.log(`
+${command} is not a valid UnitLine command.
+
+To see a list of all valid commands, please run the following command:
+    unitline help
+                `);
+    }
+}
+else {
+    console.log(helpMessage);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,119 @@
 import { error } from 'console';
 import * as distanceConverters from './unit_converters/distance'
 
-let input : string[] = process.argv
+const input : string[] = process.argv
 
-let unitType : string = input[2].slice(1)
-let currentUnit : string = input[3].split('-')[0]
-let convertedUnit : string = input[3].split('-')[1]
-let value : number = parseFloat(input[4])
+const helpMessage = `
+unitline <command>
+`
 
-type distanceUnits = 'km' | 'hm' | 'dam' | 'm' | 'dm' | 'cm' | 'mm' | 'um' | 'nm'
-let distanceConverter : Map<distanceUnits, number> = new Map([
-    ['km', distanceConverters.toKilometer(currentUnit, value)],
-    ['hm', distanceConverters.toHectometer(currentUnit, value)],
-    ['dam', distanceConverters.toDecameter(currentUnit, value)],
-    ['m', distanceConverters.toMeter(currentUnit, value)],
-    ['dm', distanceConverters.toDecimeter(currentUnit, value)],
-    ['cm', distanceConverters.toCentimeter(currentUnit, value)],
-    ['mm', distanceConverters.toMillimeter(currentUnit, value)],
-    ['um', distanceConverters.toMicrometer(currentUnit, value)],
-    ['nm', distanceConverters.toNanometer(currentUnit, value)]
+const distanceUnits : string[] = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm', 'um', 'nm']
 
-])
+const output = (unitFrom: string, value : number, unitTo: string, convertedValue : number) => {
+    console.log(`${value + unitFrom} -> ${convertedValue + unitTo}`)
+}
 
-switch (unitType) {
-    case 'distance':
-    case 'd':
-        if (distanceConverter.has(convertedUnit as distanceUnits)) {
-            console.log(`${value + currentUnit} -> ${distanceConverter.get(convertedUnit as distanceUnits) + convertedUnit}`)
-        } else {
-            throw error(`${convertedUnit} is not a valid unit`)
-        }
-        break
-    default:
-        throw error(`${unitType} is not a valid type`)
+//TODO Put all the messages in their own seperate file.
+const converter = (unitType : string, unitFrom : string, unitTo : string, value : number) : void => {
+    let convertedValue : number
+    const distanceConverter : Map<string, number> = new Map([
+        ['km', distanceConverters.toKilometer(unitFrom, value)],
+        ['hm', distanceConverters.toHectometer(unitFrom, value)],
+        ['dam', distanceConverters.toDecameter(unitFrom, value)],
+        ['m', distanceConverters.toMeter(unitFrom, value)],
+        ['dm', distanceConverters.toDecimeter(unitFrom, value)],
+        ['cm', distanceConverters.toCentimeter(unitFrom, value)],
+        ['mm', distanceConverters.toMillimeter(unitFrom, value)],
+        ['um', distanceConverters.toMicrometer(unitFrom, value)],
+        ['nm', distanceConverters.toNanometer(unitFrom, value)]
+    ])
+    switch (unitType) {
+        case 'd':
+        case 'distance':
+            if (distanceConverter.has(unitTo)) {
+                output(unitFrom, value, unitTo, distanceConverter.get(unitTo)!)
+            } else {
+                console.log(`
+${unitTo} is not a valid unit to convert to.
+
+List of valid units: ${distanceUnits.toString()}
+
+To check additional units, run:
+    unitline ls
+                    `)
+            }
+            break
+        default:
+            console.log(`
+${unitType} is not a valid type
+
+List of available types:
+d, distance       Convert units of distance
+                `)
+    }
+    
+}
+
+if (input.length >= 3) {
+    const command : string = input[2]
+    switch (command) {
+        case 'help':
+        case '--help':
+        case '-help':
+            console.log(helpMessage)
+            break
+        case 'ls':
+        case 'list':
+            const argument = input[3]
+            switch (argument) {
+                case '-d':
+                case '-distance':
+                    console.log(distanceUnits.toString())
+                    break
+                default:
+                    console.log(`
+Distance: ${distanceUnits.toString()}
+                        `)
+            }
+            break
+        case 'conv':
+        case 'convert':
+            if (input.length <= 3) {
+                console.log(`
+Not enough arguments.
+
+Usage:
+unitline conv <type of units> <unit from>-<unit to> <value>
+
+Unit Types:
+d, distance       Convert units of distance
+
+To view supported units, run:
+    unitline ls
+                    `)
+            break
+            }
+
+            let unitType : string = input[3]
+            let unitFrom : string = input[4].split('-')[0]
+            let unitTo : string = input[4].split('-')[1]
+            let value : number = parseFloat(input[5])
+
+            if (isNaN(value)) {
+                console.log(`The value provided is not a number`)
+                break
+            }
+
+            converter(unitType, unitFrom, unitTo, value)
+            break
+        default:
+            console.log(`
+${command} is not a valid UnitLine command.
+
+To see a list of all valid commands, please run the following command:
+    unitline help
+                `)
+    }
+} else {
+    console.log(helpMessage)
 }
